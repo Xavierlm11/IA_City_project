2867416b3008b0e80ded59f6dafa46048c5c7584
Akage369 <xaviercasado369@gmail.com>
2022-10-16 15:22:45 +0200

    Diglett Appears and disappears

--- MAFIA-CITY/Assets/Scenes/XAVI2/Diglett/Diglett.cs
+++ MAFIA-CITY/Assets/Scenes/XAVI2/Diglett/Diglett.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Diglett : MonoBehaviour
 {
-    [SerializeField] private float freq;
+    [SerializeField] private float freqUp;
+    [SerializeField] private float freqDown;
     private float activeTime;
 
-    [SerializeField] private GameObject ground;
+    [SerializeField] private MeshRenderer ground;
+
+    private bool hasToAppear;
+    private bool hasToDisappear;
+
+    [SerializeField] private AnimationClip appearAnim;
+    [SerializeField] private AnimationClip disappearAnim;
+    [SerializeField] private Animator anim;
+
+    private bool isUp;
 
     void Start()
     {
@@ -16,18 +26,94 @@ public class Diglett : MonoBehaviour
 
     void Update()
     {
-        activeTime += Time.deltaTime;
-        if (activeTime > freq)
+        if (isUp == true)
         {
-            activeTime -= freq;
+            activeTime += Time.deltaTime;
+            if (activeTime > freqUp)
+            {
+                activeTime -= freqUp;
 
-            Appear();
+                Disappear();
+            }
+
+
+            if (hasToDisappear == true)
+            {
+                CheckDisappear();
+            }
         }
+        else
+        {
+            activeTime += Time.deltaTime;
+            if (activeTime > freqDown)
+            {
+                activeTime -= freqDown;
+
+                Appear();
+            }
+
+            if (hasToAppear == true)
+            {
+                CheckAppear();
+            }
+        }
+
+    }
+
+    private void Disappear()
+    {
+        
+        hasToDisappear = true;
+        
     }
 
     private void Appear()
     {
-        //float x = Random.RandomRange(ground.GetComponent<BoxCollider>().bounds.x bounds.center- ground.GetComponent<MeshCollider>().bounds.);
+
+        hasToAppear = true;
+
+    }
+
+    private void CheckAppear() 
+    {       
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Appear") == true)
+        {
+            if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+            {
+                hasToAppear = false;
+                isUp = true;
+            }
+        }
+        else
+        {
+            anim.Play("Appear");
+        }
+    }
+
+    private void CheckDisappear()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Disappear") == true)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+            {
+                hasToDisappear = false;
+                isUp = false;
+                NewPos();
+            }
+        }
+        else
+        {
+            anim.Play("Disappear");
+        }
+    }
+
+    private void NewPos()
+    {
+        float x = Random.Range(ground.localBounds.min.x, ground.localBounds.extents.x);
+        float y = ground.transform.localPosition.y;
+        float z = Random.Range(ground.localBounds.min.z, ground.localBounds.extents.z);
+
+        transform.localPosition = new Vector3(x, y, z);
     }
 
 }
