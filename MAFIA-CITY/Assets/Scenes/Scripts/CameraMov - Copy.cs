1bf545c909de6518a8a8ffd2add8c79731d37a2f
Xavierlm11 <xaviloma11@gmail.com>
2022-10-16 21:39:25 +0200

    cleaning

--- MAFIA-CITY/Assets/Scenes/Scripts/CameraMov - Copy.cs
+++ MAFIA-CITY/Assets/Scenes/Scripts/CameraMov - Copy.cs
@@ -14,10 +14,10 @@ public class CameraMov : MonoBehaviour
     space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
 
 
-    float mainSpeed = 100.0f; //regular speed
+    float mainSpeed = 30.0f; //regular speed
     float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
     float maxShift = 1000.0f; //Maximum speed when holdin gshift
-    float camSens = 0.25f; //How sensitive it with mouse
+    float camSens = 0.10f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
 
