ce4acf41e8719319914b225761b3f23526aebbdf
Xavierlm11 <xaviloma11@gmail.com>
2023-05-12 22:59:22 +0200

    sounds

--- PokemonTown/Assets/Scenes/Scripts/AmbientSwap.cs
+++ PokemonTown/Assets/Scenes/Scripts/AmbientSwap.cs
@@ -7,15 +7,24 @@ public class AmbientSwap : MonoBehaviour
 {
     [SerializeField] private AudioMixerSnapshot outdoorSnap;
     [SerializeField] private AudioMixerSnapshot indorSnap;
+    [SerializeField] private AudioMixerSnapshot atentionSnap;
 
     [SerializeField] private float transitionTime = 0.25f;
+    [SerializeField] private float fastTransitionTime = 0.25f;
+    [SerializeField] private float slowTransitionTime = 0.25f;
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Center") 
         { 
-            indorSnap.TransitionTo(transitionTime); 
+            indorSnap.TransitionTo(slowTransitionTime); 
         }
+         if (collider.tag == "Atention") 
+        {
+            atentionSnap.TransitionTo(fastTransitionTime); 
+        }
+        
+
     }
 
     void OnTriggerExit(Collider collider)
@@ -23,6 +32,10 @@ public class AmbientSwap : MonoBehaviour
         if (collider.tag == "Center")
         {
             outdoorSnap.TransitionTo(transitionTime);
+        }
+        if (collider.tag == "Atention")
+        {
+            outdoorSnap.TransitionTo(fastTransitionTime);
         }
     }
 }
